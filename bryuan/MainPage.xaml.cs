dae18e230bb11b8eb7d974adfad9b5bab4011cd7
bryuan <borelabryan947@gmail.com>
2024-03-05 11:59:53 -0300

    dia 2

--- bryuan/MainPage.xaml.cs
+++ bryuan/MainPage.xaml.cs
@@ -5,9 +5,12 @@ public partial class MainPage : ContentPage
 	
 
 	public MainPage()
-	{ 
+	{
+      InitializeComponent();
 	}
-
+private void BotaoCreditos(object sender, EventArgs args)
+  {
+    frameSettings.IsVisible = true;
 
 	}
 
