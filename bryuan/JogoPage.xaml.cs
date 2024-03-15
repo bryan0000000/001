3974d985a7623100cb25e96a864fd38e99caf032
bryuan <borelabryan947@gmail.com>
2024-03-15 11:02:31 -0300

    10

--- bryuan/JogoPage.xaml.cs
+++ bryuan/JogoPage.xaml.cs
@@ -50,29 +50,92 @@ List<Historia.JogoPage> historia = new List<Historia.JogoPage>();
 
     Historia.JogoPage.Add(new HistoriaDia()
 
+  { 
+        Id = 3,
+			  Texto = "ae vo ti mostras o cara, mais soeiro da sidadi, bora la?",
+        vaiterresposta=true,
+        RespostasDoTexto01="Bora po",
+        RespostasDoTexto02="to fora irmão, mais eu topo dar um passeio.",
+        IdResposta01=4,
+        IdResposta02=5, 
+ });
+     Historia.JogoPage.Add(new HistoriaDia()
+ { 
+       
+  
+        Id = 4,
+			  Texto = "vo ti leva ate la",
+			  vaiterresposta=false,
+
+  });
+  
+     Historia.JogoPage.Add(new HistoriaDia() 
+
   {
-              Id = 3,
-			  Texto = "ae vo ti mostras o cara mais soeiro da sidadi, bora la?", 
 
-  }
+          Id = 5,
+          Texto = " ok...",
+          vaiterresposta=false,   
 
+  });
 
 
+         Historia.JogoPage.Add(new HistoriaDia() 
+   
+  {
 
+          Id = 6,
+          Texto = " eae Juba Longa quanto tempo, vim te ti avisar que tem carne nova na area ",
+          vaiterresposta=false,   
 
+  });
 
+         Historia.JogoPage.Add(new HistoriaDia() 
+   
+  {
 
+          Id = 7,
+          Texto = " serio memo, aiiiii qui dilisa",
+          vaiterresposta=true,
+          RespostasDoTexto01="e esse geitinho de baitola, por acaso eres gay?", 
+          RespostasDoTexto02="...",
+          IdResposta01=8,  
+          IdResposta02=9, 
 
+  });
 
 
+         Historia.JogoPage.Add(new HistoriaDia() 
+  {
+          Id = 8,
+          Texto = " quer morrer anão de jardin, quem tu pensas que é pra falar do meu geito em ou mindinho do lula ",
+          vaiterresposta=false,   
+          mamouotario=true,
+});
 
+        Historia.JogoPage.Add(new HistoriaDia() 
 
 
+{
+          Id = 9,
+          Texto = "mordeu a lingua fiote de crus credo, ta longe de ser digno de ser zoera como eu...",
+          vaiterresposta=true,   
+          RespostasDoTexto01="não aguentou a sentada fadinha.",
+          RespostasDoTexto02="obvio que não po ",
+          IdResposta01=10,  
+          IdResposta02=11, 
 
+ });
 
+  {
+         Historia.JogoPage.Add(new HistoriaDia() 
 
+          Id = 10,
+          Texto = "uau o cabra é zuera, e inteligente como uma porta, entretanto infelismente nao há espaço para dois nesse lindo mundo de merda...",
+          mamouotario=true,
 
 
+  });
 
 
 
@@ -93,54 +156,4 @@ List<Historia.JogoPage> historia = new List<Historia.JogoPage>();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  }
-
+}
